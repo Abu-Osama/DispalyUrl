fadc0c1c978cda3041407598e140b641da518be4
abuosamakhan <khanabuosama@gmail.com>
2016-12-19 13:13:20 +0530

    Dispaly
    hello

---
+++ app/src/main/java/com/abuosama/b_33dispalyurl/MainActivity.java
@@ -0,0 +1,22 @@
+package com.abuosama.b_33dispalyurl;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        MyFragment myFragment=new MyFragment();
+        fragmentTransaction.add(R.id.conatiner,myFragment);
+        fragmentTransaction.commit();
+
+
+    }
+}
